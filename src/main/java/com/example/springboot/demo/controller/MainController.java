8d58efdc5a145f226b6a147821d8ade3763fe64f
manat.jieranaiwongkul <manatjie19@gmail.com>
2021-09-19 15:48:09 +0700

    run robot

--- src/main/java/com/example/springboot/demo/controller/MainController.java
+++ src/main/java/com/example/springboot/demo/controller/MainController.java
@@ -3,11 +3,36 @@ package com.example.springboot.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.File;
+
 @Controller
 public class MainController {
 
     @GetMapping("/from")
     public String from(){
         return "main_from";
+    }
+
+    @GetMapping("/runtestcase")
+    public String runtestcase(){
+        run();
+        return "main_from";
+    }
+
+    private void run(){
+        try
+        {
+
+            File dir = new File("D:/robot");
+            //String cmd = "robot -v policy:"+policyNo+" -v date:"+admitDate+" -v doc:"+doctorNo+" ecs_create_claim_robot_framework.robot";
+            String cmd = "robot test_script01.txt";
+            System.out.println("cmd : "+cmd);
+            Process process = Runtime.getRuntime().exec(cmd, null, dir);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //return null;
     }
 }
