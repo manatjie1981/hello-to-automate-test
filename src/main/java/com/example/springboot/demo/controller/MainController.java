133c0bc942c0e382d00d8e8403f2dd9a21cae8fd
manat.jieranaiwongkul <manatjie19@gmail.com>
2021-09-22 22:08:49 +0700

    fixbug  read result

--- src/main/java/com/example/springboot/demo/controller/MainController.java
+++ src/main/java/com/example/springboot/demo/controller/MainController.java
@@ -2,12 +2,23 @@ package com.example.springboot.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class MainController {
 
+    private  String FILENAME_OUTPUT = "D:/robot/output.xml";
     @GetMapping("/from")
     public String from(){
         return "main_from";
@@ -16,6 +27,9 @@ public class MainController {
     @GetMapping("/runtestcase")
     public String runtestcase(){
         run();
+        try{Thread.sleep(60000);}catch(InterruptedException e){System.out.println(e);}
+        readResultExecute();
+
         return "main_from";
     }
 
@@ -34,5 +48,57 @@ public class MainController {
         }
 
         //return null;
+    }
+
+    public Boolean readResultExecute() {
+
+        // Instantiate the Factory
+        Boolean isPass = Boolean.FALSE;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            // optional, but recommended
+            // process XML securely, avoid attacks like XML External Entities (XXE)
+            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+            // parse XML file
+            dbf.setValidating(false);
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(FILENAME_OUTPUT));
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
+            System.out.println("------");
+
+            // get <staff>
+            NodeList list = doc.getElementsByTagName("stat");
+
+            for (int temp = 0; temp < list.getLength(); temp++) {
+
+                Node node = list.item(temp);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element element = (Element) node;
+
+                    // get staff's attribute
+                    String pass = element.getAttributes().getNamedItem("pass").getTextContent();
+                    System.out.println(" pass : "+pass);
+                    String fail = element.getAttributes().getNamedItem("fail").getTextContent();
+                    System.out.println(" fail : "+fail);
+                    String skip = element.getAttributes().getNamedItem("skip").getTextContent();
+                    System.out.println(" skip : "+skip);
+
+
+                }
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return  isPass;
     }
 }
